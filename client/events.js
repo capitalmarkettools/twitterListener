b08c295e5df89ceeb5ff186dc4f26c6800113930
capitalmarkettools <capitalmarkettools@users.noreply.github.com>
2016-03-13 22:11:43 -0400

    Added files via upload

---
+++ client/events.js
@@ -0,0 +1,11 @@
+/**
+ * Created by cmt on 3/13/16.
+ */
+Template.listener.events({
+    'click #start': function () {
+        alert("Start clicked.");
+    },
+    'click #stop': function () {
+        alert("Stop clicked.");
+    }
+});
